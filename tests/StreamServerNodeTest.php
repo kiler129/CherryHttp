0afc733b0240f7d9b54310183e824fdfbc0f013b
kiler129 <grzegorz@noflash.pl>
2015-06-05 14:59:16 +0200

    Added logger mock.

--- tests/StreamServerNodeTest.php
+++ tests/StreamServerNodeTest.php
@@ -5,6 +5,16 @@ class StreamServerNodeTest extends \PHPUnit_Framework_TestCase {
 
     const CLASS_NAME = '\noFlash\CherryHttp\StreamServerNode';
 
+    /**
+     * @var StreamServerNode.php:38
+     */
+    private $loggerMock;
+
+    public function setUp()
+    {
+        $this->loggerMock = $this->getMockForAbstractClass('\Psr\Log\LoggerInterface');
+    }
+
     public function testImplementsStreamServerNodeInterface()
     {
         $streamServerNodeReflection = new \ReflectionClass(self::CLASS_NAME);
