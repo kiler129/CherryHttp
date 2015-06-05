4348ffaaaa25cc155b27dcb0b1cee6568463af2e
kiler129 <grzegorz@noflash.pl>
2015-06-05 02:07:19 +0200

    Added mock for StreamServerNode inside NodeDisconnectExceptionTest.

--- tests/NodeDisconnectExceptionTest.php
+++ tests/NodeDisconnectExceptionTest.php
@@ -4,9 +4,20 @@ namespace noFlash\CherryHttp;
 
 class NodeDisconnectExceptionTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @var StreamServerNodeInterface
+     */
+    private $nodeMock;
+
+    public function setUp()
+    {
+        $this->nodeMock = $this->getMockBuilder('\noFlash\CherryHttp\StreamServerNodeInterface')->getMock();
+    }
+
     public function testClassExtendsException()
     {
         $serverExceptionReflection = new \ReflectionClass('\noFlash\CherryHttp\NodeDisconnectException');
         $this->assertTrue($serverExceptionReflection->isSubclassOf('\Exception'));
     }
+
 }
