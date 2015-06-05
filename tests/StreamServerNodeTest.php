f3d72efc412d111853dda4be34e706b6de29cee3
kiler129 <grzegorz@noflash.pl>
2015-06-05 14:20:35 +0200

    Added test for StreamServerNode interface implementation.

--- tests/StreamServerNodeTest.php
+++ tests/StreamServerNodeTest.php
@@ -1,6 +1,12 @@
 <?php
 namespace noFlash\CherryHttp;
 
-class StreamServerNodeTest extends PHPUnit_Framework_TestCase {
+class StreamServerNodeTest extends \PHPUnit_Framework_TestCase {
+
+    public function testImplementsStreamServerNodeInterface()
+    {
+        $streamServerNodeReflection = new \ReflectionClass('\noFlash\CherryHttp\StreamServerNode');
+        $this->assertTrue($streamServerNodeReflection->isSubclassOf('\noFlash\CherryHttp\StreamServerNodeInterface'));
+    }
 
 }
