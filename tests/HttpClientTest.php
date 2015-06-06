f52e40dd07753f7138d5057135522c819b268940
kiler129 <grzegorz@noflash.pl>
2015-06-07 00:14:44 +0200

    Added test for HttpClient interface implementation.

--- tests/HttpClientTest.php
+++ tests/HttpClientTest.php
@@ -3,5 +3,9 @@ namespace noFlash\CherryHttp;
 
 
 class HttpClientTest extends \PHPUnit_Framework_TestCase {
-
+    public function testImplementsStreamServerNodeInterface()
+    {
+        $streamServerNodeReflection = new \ReflectionClass('\noFlash\CherryHttp\HttpClient');
+        $this->assertTrue($streamServerNodeReflection->isSubclassOf('\noFlash\CherryHttp\StreamServerNodeInterface'));
+    }
 }
