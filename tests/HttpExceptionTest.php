302cb8ebcec5320b273112d340883df39691199a
kiler129 <grzegorz@noflash.pl>
2015-06-07 01:58:59 +0200

    Added test checking if HttpExceptions extends Exception.

--- tests/HttpExceptionTest.php
+++ tests/HttpExceptionTest.php
@@ -3,4 +3,9 @@ namespace noFlash\CherryHttp;
 
 
 class HttpExceptionTest extends \PHPUnit_Framework_TestCase {
+    public function testClassExtendsException()
+    {
+        $serverExceptionReflection = new \ReflectionClass('\noFlash\CherryHttp\HttpException');
+        $this->assertTrue($serverExceptionReflection->isSubclassOf('\Exception'));
+    }
 }
