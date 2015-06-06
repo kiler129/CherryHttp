8918d89b58a010c1eff0ca55f7b78ebf538740b4
kiler129 <grzegorz@noflash.pl>
2015-06-06 16:10:47 +0200

    Added test for StreamServerNode::onWriteReady().

--- tests/StreamServerNodeTest.php
+++ tests/StreamServerNodeTest.php
@@ -1,6 +1,8 @@
 <?php
 namespace noFlash\CherryHttp;
 
+use org\bovigo\vfs\vfsStream;
+
 class StreamServerNodeTest extends \PHPUnit_Framework_TestCase {
 
     const CLASS_NAME = '\noFlash\CherryHttp\StreamServerNode';
@@ -215,4 +217,43 @@ class StreamServerNodeTest extends \PHPUnit_Framework_TestCase {
         $this->assertTrue($streamServerNode->isWriteReady());
     }
 
+    public function testOnWriteReadyReturnsTrueWhenFullBufferHasBeenWritten()
+    {
+        $stream = fopen((defined('PHP_WINDOWS_VERSION_MAJOR')) ? 'nul' : '/dev/null', 'w');
+
+        $streamServerNode = $this->getMockForAbstractClass(self::CLASS_NAME,
+            array($stream, null, $this->loggerMock)
+        );
+
+        $streamServerNode->pushData('123456789');
+        $this->assertTrue($streamServerNode->onWriteReady());
+    }
+
+    /**
+     * @short
+     */
+    public function testOnWriteReadyProperlyHandlesStreamBlockage()
+    {
+        $vfsRoot = vfsStream::setup();
+        vfsStream::setQuota(2);
+        $streamMock = vfsStream::newFile('test')->withContent(' ')->at($vfsRoot); //Using vfsStream for mocking network sockets has one drawbacks - it's a file, so feof() will return true, some content prevents that
+        $stream = fopen($streamMock->url(), 'r+');
+        rewind($stream);
+
+        $streamServerNode = $this->getMockForAbstractClass(self::CLASS_NAME,
+            array($stream, null, $this->loggerMock)
+        );
+
+        $streamServerNode->pushData('123456789');
+        $this->assertFalse($streamServerNode->onWriteReady(), 'onWriteReady() didn\'t returned false on 1st try'); //This should write two bytes and return false (== not all data was written)
+        $this->assertSame('12', $streamMock->getContent(), 'Stream should get first two bytes');
+
+        vfsStream::setQuota(4); //Allow another two bytes to be written
+        $this->assertFalse($streamServerNode->onWriteReady(), 'onWriteReady() didn\'t returned false on 2nd try');
+        $this->assertSame('1234', $streamMock->getContent(), 'Stream should get next two bytes');
+
+        vfsStream::setQuota(PHP_INT_MAX); //Allow all bytes to pass
+        $this->assertTrue($streamServerNode->onWriteReady(), 'onWriteReady() didn\'t returned true on 3rd try');
+        $this->assertSame('123456789', $streamMock->getContent(), 'Stream should get remaining bytes');
+    }
 }
