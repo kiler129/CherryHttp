27a8803a19fa8a0b69c2649f4b1687daaaae9a72
kiler129 <grzegorz@noflash.pl>
2015-06-07 02:00:33 +0200

    Added test for response generation without parameters in HttpException.

--- tests/HttpExceptionTest.php
+++ tests/HttpExceptionTest.php
@@ -8,4 +8,11 @@ class HttpExceptionTest extends \PHPUnit_Framework_TestCase {
         $serverExceptionReflection = new \ReflectionClass('\noFlash\CherryHttp\HttpException');
         $this->assertTrue($serverExceptionReflection->isSubclassOf('\Exception'));
     }
+
+    public function testResponseIsGeneratedWithoutAnyParameters()
+    {
+        $httpException = new HttpException();
+        $this->assertInstanceOf('\noFlash\CherryHttp\HttpResponse', $httpException->getResponse());
+    }
+
 }
