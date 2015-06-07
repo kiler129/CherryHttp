1d01e7580b0431928fa9e4b564ecd577ce128ce6
kiler129 <grzegorz@noflash.pl>
2015-06-07 23:29:24 +0200

    Added test checking if HttpMessage by default returns HTTP/1.1 as version.

--- tests/HttpMessageTest.php
+++ tests/HttpMessageTest.php
@@ -4,4 +4,18 @@ namespace noFlash\CherryHttp;
 
 class HttpMessageTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * @return HttpMessage|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private function getHttpMessageObject()
+    {
+        return $this->getMockForAbstractClass('\noFlash\CherryHttp\HttpMessage');
+    }
+
+    public function testDefaultMessageIsCreatedWithNewestProtocolVersion()
+    {
+        $httpMessage = $this->getHttpMessageObject();
+
+        $this->assertEquals('1.1', $httpMessage->getProtocolVersion());
+    }
 }
