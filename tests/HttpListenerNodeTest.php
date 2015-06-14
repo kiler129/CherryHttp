7087f160fbdfae96ff0d71dd7995ae78c89f4370
kiler129 <grzegorz@noflash.pl>
2015-06-15 01:14:29 +0200

    Added test checking if HttpListenerNode IP validation.

--- tests/HttpListenerNodeTest.php
+++ tests/HttpListenerNodeTest.php
@@ -2,7 +2,8 @@
 namespace noFlash\CherryHttp;
 
 
-class HttpListenerNodeTest extends \PHPUnit_Framework_TestCase {
+class HttpListenerNodeTest extends \PHPUnit_Framework_TestCase
+{
     /**
      * @testdox Class extends StreamServerNode
      */
@@ -10,5 +11,47 @@ class HttpListenerNodeTest extends \PHPUnit_Framework_TestCase {
     {
         $clientUpgradeExceptionReflection = new \ReflectionClass('\noFlash\CherryHttp\HttpListenerNode');
         $this->assertTrue($clientUpgradeExceptionReflection->isSubclassOf('\noFlash\CherryHttp\StreamServerNode'));
+    }
+
+    public function validIpsProvider()
+    {
+        return array(
+            array('127.0.0.1'),
+            array('::1'),
+            array('::ffff:127.0.0.1'),
+            array('0.0.0.0')
+        );
+    }
+
+    /**
+     * @dataProvider validIpsProvider
+     */
+    public function testConstructorAcceptsValidIps($ip)
+    {
+        $serverMock = $this->getMockBuilder('\noFlash\CherryHttp\Server')->getMock();
+
+        $listener = new HttpListenerNode($serverMock, $ip);
+        $this->assertSame($ip, $listener->getIp());
+    }
+
+    public function invalidIpsProvider()
+    {
+        return array(
+            array('localhost'),
+            array('127.0.0.1:8080'),
+            array('127,0,0,1'),
+            array('999.999.999.999')
+        );
+    }
+
+    /**
+     * @dataProvider invalidIpsProvider
+     */
+    public function testConstructorRejectsInvalidIps($ip)
+    {
+        $serverMock = $this->getMockBuilder('\noFlash\CherryHttp\Server')->getMock();
+
+        $this->setExpectedException('\InvalidArgumentException');
+        new HttpListenerNode($serverMock, $ip);
     }
 }
