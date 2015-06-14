340ddba685cfb68beca611d8b5449bbb361402b1
kiler129 <grzegorz@noflash.pl>
2015-06-15 00:54:49 +0200

    Added test checking if HttpListenerNode extends StreamServerNode.

--- tests/HttpListenerNodeTest.php
+++ tests/HttpListenerNodeTest.php
@@ -3,4 +3,12 @@ namespace noFlash\CherryHttp;
 
 
 class HttpListenerNodeTest extends \PHPUnit_Framework_TestCase {
+    /**
+     * @testdox Class extends StreamServerNode
+     */
+    public function testClassExtendsStreamServerNode()
+    {
+        $clientUpgradeExceptionReflection = new \ReflectionClass('\noFlash\CherryHttp\HttpListenerNode');
+        $this->assertTrue($clientUpgradeExceptionReflection->isSubclassOf('\noFlash\CherryHttp\StreamServerNode'));
+    }
 }
