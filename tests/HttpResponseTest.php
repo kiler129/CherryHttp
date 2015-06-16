8524029a3c8594afc0f13f1ec74916b7d2f529e4
kiler129 <grzegorz@noflash.pl>
2015-06-16 22:13:23 +0200

    Added test for default HTTP code used by HttpResponse.

--- tests/HttpResponseTest.php
+++ tests/HttpResponseTest.php
@@ -3,4 +3,10 @@ namespace noFlash\CherryHttp;
 
 
 class HttpResponseTest extends \PHPUnit_Framework_TestCase {
+    public function testResponseUsesHttpOkCodeByDefault()
+    {
+        $httpResponse = new HttpResponse;
+
+        $this->assertSame(HttpCode::OK, $httpResponse->getCode());
+    }
 }
