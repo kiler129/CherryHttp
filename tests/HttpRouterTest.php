48496af6ff36919e5f7d8b793a121407afa733dd
kiler129 <grzegorz@noflash.pl>
2015-06-19 15:43:33 +0200

    Added test for HttpRouter interface implementation.

--- tests/HttpRouterTest.php
+++ tests/HttpRouterTest.php
@@ -3,4 +3,9 @@ namespace noFlash\CherryHttp;
 
 
 class HttpRouterTest extends \PHPUnit_Framework_TestCase {
+    public function testClassImplementsHttpRouterInterface()
+    {
+        $serverExceptionReflection = new \ReflectionClass('\noFlash\CherryHttp\HttpRouter');
+        $this->assertTrue($serverExceptionReflection->isSubclassOf('\noFlash\CherryHttp\HttpRouterInterface'));
+    }
 }
