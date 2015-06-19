7460456fbe2da821f0ef2c89e71b73462413c553
kiler129 <grzegorz@noflash.pl>
2015-06-19 22:42:50 +0200

    Added test for default router in Server.

--- tests/ServerTest.php
+++ tests/ServerTest.php
@@ -3,5 +3,18 @@ namespace noFlash\CherryHttp;
 
 
 class ServerTest extends \PHPUnit_Framework_TestCase {
+    /**
+     * @var Server
+     */
+    private $server;
 
+    public function setUp()
+    {
+        $this->server = new Server();
+    }
+
+    public function testIfNoRouterWasGivenDefaultIsUsed()
+    {
+        $this->assertInstanceOf('\noFlash\CherryHttp\HttpRouter', $this->server->router);
+    }
 }
