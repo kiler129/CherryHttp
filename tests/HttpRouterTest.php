22925f613a6c3cbda318057eb48ab97038edef47
kiler129 <grzegorz@noflash.pl>
2015-06-19 15:55:35 +0200

    Added test for adding path handler to HttpRouter.

--- tests/HttpRouterTest.php
+++ tests/HttpRouterTest.php
@@ -3,9 +3,37 @@ namespace noFlash\CherryHttp;
 
 
 class HttpRouterTest extends \PHPUnit_Framework_TestCase {
+    /**
+     * @var HttpRouter
+     */
+    private $httpRouter;
+
+    public function setUp()
+    {
+        $logger = $this->getMockForAbstractClass('\Psr\Log\LoggerInterface');
+        $this->httpRouter = new HttpRouter($logger);
+    }
+
     public function testClassImplementsHttpRouterInterface()
     {
         $serverExceptionReflection = new \ReflectionClass('\noFlash\CherryHttp\HttpRouter');
         $this->assertTrue($serverExceptionReflection->isSubclassOf('\noFlash\CherryHttp\HttpRouterInterface'));
+    }
+
+    public function testPathHandlerCanBeAdded()
+    {
+        $requestHandler = $this->getMockBuilder('\noFlash\CherryHttp\HttpRequestHandlerInterface')->getMock();
+        $requestHandler
+            ->expects($this->atLeastOnce())
+            ->method('getHandledPaths')
+            ->willReturn(array('/test'));
+
+        $this->httpRouter->addPathHandler($requestHandler);
+
+        $routerReflection = new \ReflectionObject($this->httpRouter);
+        $pathHandlersReflection = $routerReflection->getProperty('pathHandlers');
+        $pathHandlersReflection->setAccessible(true);
+        $handledPaths = $pathHandlersReflection->getValue($this->httpRouter);
+        $this->assertSame(array('/test' => $requestHandler), $handledPaths);
     }
 }
