d65f0a954faf0295e646dde5fca632b50fa217e7
kiler129 <grzegorz@noflash.pl>
2015-06-19 22:48:11 +0200

    Added test for default logger in Server.

--- tests/ServerTest.php
+++ tests/ServerTest.php
@@ -13,6 +13,15 @@ class ServerTest extends \PHPUnit_Framework_TestCase {
         $this->server = new Server();
     }
 
+    public function testIfNoLoggerWasGivenDefaultIsUsed()
+    {
+        $serverReflection = new \ReflectionObject($this->server);
+        $loggerReflection = $serverReflection->getProperty('logger');
+        $loggerReflection->setAccessible(true);
+
+        $this->assertInstanceOf('\Psr\Log\NullLogger', $loggerReflection->getValue($this->server));
+    }
+
     public function testIfNoRouterWasGivenDefaultIsUsed()
     {
         $this->assertInstanceOf('\noFlash\CherryHttp\HttpRouter', $this->server->router);
