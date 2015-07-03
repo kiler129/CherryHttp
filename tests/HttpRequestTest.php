00ddb471256a637618a447a2e1b47bf262964213
kiler129 <grzegorz@noflash.pl>
2015-07-03 23:26:50 +0200

    Added test for empty request text in HttpRequest.

--- tests/HttpRequestTest.php
+++ tests/HttpRequestTest.php
@@ -3,9 +3,26 @@ namespace noFlash\CherryHttp;
 
 
 class HttpRequestTest extends \PHPUnit_Framework_TestCase {
+    /**
+     * @var \Psr\Log\LoggerInterface
+     */
+    private $loggerMock;
+
+
+    public function setUp()
+    {
+        $this->loggerMock = $this->getMockBuilder('\Psr\Log\LoggerInterface')->getMock();
+    }
+
     public function testClassExtendsHttpMessage()
     {
         $clientUpgradeExceptionReflection = new \ReflectionClass('\noFlash\CherryHttp\HttpRequest');
         $this->assertTrue($clientUpgradeExceptionReflection->isSubclassOf('\noFlash\CherryHttp\HttpMessage'));
+    }
+
+    public function testEmptyRequestIsRejected()
+    {
+        $this->setExpectedException('\noFlash\CherryHttp\HttpException');
+        new HttpRequest('', $this->loggerMock);
     }
 }
