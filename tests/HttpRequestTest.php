1f42f8ad3ec696148ef9d2559a69dd3361c38835
kiler129 <grzegorz@noflash.pl>
2015-07-03 21:49:45 +0200

    Added test for extending HttpMessage by HttpRequest.

--- tests/HttpRequestTest.php
+++ tests/HttpRequestTest.php
@@ -3,4 +3,9 @@ namespace noFlash\CherryHttp;
 
 
 class HttpRequestTest extends \PHPUnit_Framework_TestCase {
+    public function testClassExtendsHttpMessage()
+    {
+        $clientUpgradeExceptionReflection = new \ReflectionClass('\noFlash\CherryHttp\HttpRequest');
+        $this->assertTrue($clientUpgradeExceptionReflection->isSubclassOf('\noFlash\CherryHttp\HttpMessage'));
+    }
 }
