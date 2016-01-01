2206068ebe30f7cee717babc510612f090b21442
kiler129 <grzegorz@noflash.pl>
2016-01-01 21:24:51 +0100

    Implemented BufferOverflowException.

--- src/IO/Exception/BufferOverflowException.php
+++ src/IO/Exception/BufferOverflowException.php
@@ -12,7 +12,46 @@
 
 namespace noFlash\CherryHttp\IO\Exception;
 
+/**
+ * Exception used to indicate overflow on any kind of buffers.
+ */
 class BufferOverflowException extends \OverflowException
 {
+    /**
+     * @var
+     */
+    private $overflowMagnitude;
+
+    /**
+     * Returns information how big (if known) that overflow was.
+     *
+     * @return number|null Will return positive numeric value (depending on the context) or null if overflow magnitude
+     *                     is unknown.
+     */
+    public function getOverflowMagnitude()
+    {
+        return $this->overflowMagnitude;
+    }
+
+    /**
+     * Sets how big overflow is.
+     *
+     * @param number $magnitude Positive number.
+     *
+     * @throws \InvalidArgumentException Non-numeric magnitude specified.
+     * @throws \LogicException Negative magnitude specified.
+     */
+    public function setOverflowMagnitude($magnitude)
+    {
+        if ($magnitude < 0) {
+            throw new \LogicException('Overflow magnitude cannot be negative.');
+        }
+
+        if (!is_numeric($magnitude)) {
+            throw new \InvalidArgumentException('Overflow magnitude should be a number.');
+        }
+
+        $this->overflowMagnitude = $magnitude;
+    }
 
 }
---
+++ tests/IO/Exception/BufferOverflowExceptionTest.php
@@ -0,0 +1,69 @@
+<?php
+/*
+ * This file is part of CherryHttp project.
+ * You are using it at your own risk and you are fully responsible
+ *  for everything that code will do.
+ *
+ * (c) Grzegorz Zdanowski <grzegorz@noflash.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace noFlash\CherryHttp\Tests\IO\Exception;
+
+use noFlash\CherryHttp\IO\Exception\BufferOverflowException;
+
+class BufferOverflowExceptionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var BufferOverflowException
+     */
+    private $subjectUnderTest;
+
+    public function setUp()
+    {
+        $this->subjectUnderTest = new BufferOverflowException();
+    }
+
+    public function testClassExtendsCorrectExceptionClasses()
+    {
+        $serverExceptionReflection = new \ReflectionClass(BufferOverflowException::class);
+        $this->assertTrue($serverExceptionReflection->isSubclassOf('\OverflowException'));
+        $this->assertTrue($serverExceptionReflection->isSubclassOf('\Exception'));
+    }
+
+    public function testOverflowMagnitudeIsNullByDefault()
+    {
+        $this->assertNull($this->subjectUnderTest->getOverflowMagnitude());
+    }
+
+    public function testOverflowMagnitudeIsHeldByObject()
+    {
+        $this->subjectUnderTest->setOverflowMagnitude(123);
+        $this->assertSame(123, $this->subjectUnderTest->getOverflowMagnitude());
+
+        $this->subjectUnderTest->setOverflowMagnitude(333);
+        $this->assertSame(333, $this->subjectUnderTest->getOverflowMagnitude());
+
+        $this->subjectUnderTest->setOverflowMagnitude(999999);
+        $this->assertSame(999999, $this->subjectUnderTest->getOverflowMagnitude());
+
+        $this->subjectUnderTest->setOverflowMagnitude(PHP_INT_MAX);
+        $this->assertSame(PHP_INT_MAX, $this->subjectUnderTest->getOverflowMagnitude());
+    }
+
+    public function testOverflowMagnitudeRejectsNonNumericValues()
+    {
+        $this->setExpectedException('\InvalidArgumentException', 'Overflow magnitude should be a number.');
+
+        $this->subjectUnderTest->setOverflowMagnitude([42]);
+    }
+
+    public function testOverflowMagnitudeRejectsNegativeValues()
+    {
+        $this->setExpectedException('\LogicException', 'Overflow magnitude cannot be negative.');
+
+        $this->subjectUnderTest->setOverflowMagnitude(-1);
+    }
+}
