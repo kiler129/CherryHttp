70e7cc0624d67443cb454aa44edfc6ca427d0241
kiler129 <grzegorz@noflash.pl>
2016-03-20 00:34:19 -0500

    Added stub of stream abstraction.

---
+++ src/IO/Stream/StreamInterface.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This file is part of CherryHttp project.
+ * You are using it at your own risk and you are fully responsible for everything that code will do.
+ *
+ * Copyright (c) 2016 Grzegorz Zdanowski <grzegorz@noflash.pl>
+ *
+ * For the full copyright and license information, please view the LICENSE file distributed with this source code.
+ */
+
+namespace noFlash\CherryHttp\IO\Stream;
+
+/**
+ * Represents abstract stream of any data.
+ *
+ * Note: this interface is not ready yet - it's just a stub. It will be probably very similar (identical?) to the PSR-7.
+ */
+interface StreamInterface
+{
+
+}
